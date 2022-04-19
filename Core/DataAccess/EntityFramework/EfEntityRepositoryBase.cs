ee6f254ee8b998192f73d267369525d74b9cf683
Csper45 <Csper45@DESKTOP-KTI1DF2>
2022-04-19 15:47:11 +0300

    190422

--- Core/DataAccess/EntityFramework/EfEntityRepositoryBase.cs
+++ Core/DataAccess/EntityFramework/EfEntityRepositoryBase.cs
@@ -3,18 +3,89 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Core.DataAccess.EntityFramework
 {
     //Context : db tabloları ile proje classları bağlamak 
-    public class EfEntityRepositoryBase<TEntity, TContext>
+    public class EfEntityRepositoryBase<TEntity, TContext> : IEntityRepository<TEntity>
         where TEntity: class, IEntity, new()
         where TContext : DbContext, new()
 
     {
-       
+        public void Add(TEntity entity)
+        {
+            //buradaki using : IDisposable pattern implementation of c# , denilen bir c# dökümantasyonu 
+            using (TContext context = new TContext())
+            {
+                //işlem bitince array i hızlıca temizler
+
+                // gönderilen veri kaynağından bir bir veri ile eşleştirmek için kullanılır 
+
+                //state durum demek
+
+                var addedEntity = context.Entry(entity);  //referansı yakala
+                addedEntity.State = EntityState.Added;   //eklenecek nesne 
+                context.SaveChanges();                   //ekle
+               
+                //savechanges işlem gerçekleştirir     
+
+            }
+        }
+
+        public void Delete(TEntity entity)
+        {
+
+            using (TContext context = new TContext())
+            {
+
+                var deletedEntity = context.Entry(entity);
+                deletedEntity.State = EntityState.Deleted;
+                context.SaveChanges();
+
+
+            }
+
+
+
+        }
+
+        public TEntity Get(Expression<Func<TEntity, bool>> filter)
+        {
+            //bu tek data getirir
+            using (TContext context = new TContext())
+            {
+                return context.Set<TEntity>().SingleOrDefault(filter);
+
+                // singleofdefault belirtilen koşulda sadece bir eleman getirir
+            }
+        }
+
+        public List<TEntity> GetAll(Expression<Func<TEntity, bool>> filter = null)
+        {
+            //bu tüm dataları getirir
+            using (TContext context = new TContext())
+            {
+                return filter==null 
+                    ? context.Set<TEntity>().ToList() //null ise bu komutu döndür
+                    : context.Set<TEntity>().Where(filter).ToList();  // değilse bu komutu döndür  //where filtreyi çeker 
+            }
+        }
+
+        public void Update(TEntity entity)
+        {
+            using (TContext context = new TContext())
+            {
+
+                var updatedEntity = context.Entry(entity);
+                updatedEntity.State = EntityState.Modified;
+                context.SaveChanges();
+
+
+            }
+        }
         
 
     }
--- DataAccess/Concrete/EntityFramework/EfProductDal.cs
+++ DataAccess/Concrete/EntityFramework/EfProductDal.cs
@@ -1,4 +1,5 @@
-﻿using DataAccess.Abstract;
+﻿using Core.DataAccess.EntityFramework;
+using DataAccess.Abstract;
 using Entities.Concrete;
 using Microsoft.EntityFrameworkCore;
 using System;
@@ -13,78 +14,9 @@ namespace DataAccess.Concrete.EntityFramework
     //nuget
     //veritabanı işlemi yapma süreci: entity framework
 
-    public class EfProductDal : IProductDal
+    //IproductDal için gerekenler EfEntityRepositoyryBasenin içinde 
+    public class EfProductDal : EfEntityRepositoryBase<Product, NorthwindContext>, IProductDal
     {
-        public void Add(Product entity)
-        {
-            //buradaki using : IDisposable pattern implementation of c# , denilen bir c# dökümantasyonu 
-            using (NorthwindContext context = new NorthwindContext())
-            {
-                //işlem bitince array i hızlıca temizler
-
-                // gönderilen veri kaynağından bir bir veri ile eşleştirmek için kullanılır 
-
-                //state durum demek
-
-                var addedEntity = context.Entry(entity);  //referansı yakala
-                addedEntity.State = EntityState.Added;   //eklenecek nesne 
-                context.SaveChanges();                   //ekle
-               
-                //savechanges işlem gerçekleştirir     
-
-            }
-        }
-
-        public void Delete(Product entity)
-        {
-
-            using (NorthwindContext context = new NorthwindContext())
-            {
-
-                var deletedEntity = context.Entry(entity);
-                deletedEntity.State = EntityState.Deleted;
-                context.SaveChanges();
-
-
-            }
-
-
-
-        }
-
-        public Product Get(Expression<Func<Product, bool>> filter)
-        {
-            //bu tek data getirir
-            using (NorthwindContext context = new NorthwindContext())
-            {
-                return context.Set<Product>().SingleOrDefault(filter);
-
-                // singleofdefault belirtilen koşulda sadece bir eleman getirir
-            }
-        }
-
-        public List<Product> GetAll(Expression<Func<Product, bool>> filter = null)
-        {
-            //bu tüm dataları getirir
-            using (NorthwindContext context = new NorthwindContext())
-            {
-                return filter==null 
-                    ? context.Set<Product>().ToList() //null ise bu komutu döndür
-                    : context.Set<Product>().Where(filter).ToList();  // değilse bu komutu döndür  //where filtreyi çeker 
-            }
-        }
-
-        public void Update(Product entity)
-        {
-            using (NorthwindContext context = new NorthwindContext())
-            {
-
-                var updatedEntity = context.Entry(entity);
-                updatedEntity.State = EntityState.Modified;
-                context.SaveChanges();
-
-
-            }
-        }
+       
     }
 }
